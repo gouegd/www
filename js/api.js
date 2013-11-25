f7a89b6b83fc75e47e2e27ca8288440e8cd16c79
Gregory Desfour <greg.desfour@kiwiplan.co.nz>
2013-11-26 00:33:56 +1300

    First map display

--- js/api.js
+++ js/api.js
@@ -1,15 +1,32 @@
 define(['reqwest'], function(reqwest) {
   
-  return {
-
-    getTags: function() {
-      return reqwest({
-        url: 'http://api.flickr.com/services/rest/?method=flickr.tags.getListUser&api_key=1eee600a4243513b3894294794e44169&user_id=61601734%40N00&format=json'
+  var reqwestFlickrJson = function(params){
+    return reqwest({
+        url: 'http://api.flickr.com/services/rest/'
       , type: 'jsonp'
       , jsonpCallbackName: 'jsonFlickrApi'
+      , data: _.extend(params, {
+          api_key: '1eee600a4243513b3894294794e44169',
+          user_id: '61601734@N00',
+          format: 'json'
+        })
+      });
+  };
+
+  return {
+
+    getTags: function(){
+      return reqwestFlickrJson({
+        method: 'flickr.tags.getListUser'
+      });
+    },
+
+    getSets: function(){
+      return reqwestFlickrJson({
+        method: 'flickr.collections.getTree',
+        collection_id: '6556115-72157635171144622'
       });
     }
   }
-
 
 });
--- js/start.js
+++ js/start.js
@@ -1,16 +1,71 @@
-require(['domReady!', 'lodash', 'api', 'ractive', 'text!templates/tags.html'],
-    function(doc, _, api, R, t_tags) {
+require(['domReady!', 'lodash', 'api', 'ractive', 'leaflet', 'text!templates/sets.html'],
+    function (doc, _, api, R, L, t_sets) {
   
-  var tags = new R({
-    el: 'tags',
-    template: t_tags
+  var RE_LL = /^LL:(-?[\d\.]+),(-?[\d\.]+)/;
+
+  var sets = new R({
+    el: 'sets',
+    template: t_sets
   });
 
-  api.getTags().then(function (resp) {
-    var allTags = _.map(resp.who.tags.tag, function(tag){
-      return {name: tag._content};
+  var map = L.map('map');
+  L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
+    attribution: '<a href="http://openstreetmap.org">OpenStreetMap</a> | <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 16,
+    key: 'de8efc7432d34bb6ae69673b27cf52f3',
+    styleId: '114653'
+  }).addTo(map);
+
+  var moveMap = function moveMap (latlon) {
+    map.setView(latlon, 6, {animate: true});
+    var circle = L.circleMarker(latlon, {
+      color: 'red'
+    }).addTo(map);
+  };
+
+  api.getSets().then(function (resp) {
+    console.dir(resp);
+    var kiwiCollection = _.find(resp.collections.collection, function(collection){
+      return collection.title === 'New Zealand';
     });
-    tags.set({tags: allTags});
+
+    var kiwiSets = kiwiCollection.set;
+
+    _.each(kiwiSets, function (set) {
+      var result = RE_LL.exec(set.description);
+      if (!result) {
+        return;
+      }
+
+      var lat = +result[1];
+      var lon = +result[2];
+      if (Number.isNaN(lat) || Number.isNaN(lon)) {
+        return;
+      }
+
+      var latlon = set.latlon = [lat, lon];
+
+      console.log(latlon);
+    });
+
+    sets.set({sets: kiwiSets});
+
+    sets.on('locate', function (event) {
+      // `this` is the ractive
+      // `event` contains information about the proxy event
+      var id = event.node.getAttribute('data-setid');
+      var sets = this.get('sets');
+      var setToLocate = _.find(sets, function(set) {
+        return set.id === id;
+      });
+
+      if (!setToLocate || !setToLocate.latlon) {
+        return;
+      }
+
+      moveMap(setToLocate.latlon);
+    });
+
   });
 
 });
